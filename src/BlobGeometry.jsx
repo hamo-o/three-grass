f7d27b9acc7c1ca024fdbdc5e02a90d6e3f0e4ae
hamo-o <yheel369@gmail.com>
2022-12-02 22:58:24 +0900

    feat: mapping 시도

--- src/BlobGeometry.jsx
+++ src/BlobGeometry.jsx
@@ -5,6 +5,8 @@ import Perlin from 'perlin.js'
 
 Perlin.seed(Math.random())
 
+import { SVGLoader } from 'three/examples/jsm/loaders/SVGLoader'
+
 const computeFlowerDensity = (geometry) => {
   const position = geometry.getAttribute('position')
   const density = []
@@ -23,30 +25,41 @@ const computeFlowerDensity = (geometry) => {
 export const BlobGeometry = forwardRef((props, ref) => {
   const geom = useRef()
 
-  useEffect(() => {
-    const vertex = new THREE.Vector3()
-    const normal = new THREE.Vector3()
-    let newPositionAttribute = []
-    const positionAttribute = geom.current.getAttribute('position')
-    const normalAttribute = geom.current.getAttribute('normal')
-    for (let i = 0; i < positionAttribute.count; i++) {
-      vertex.fromBufferAttribute(positionAttribute, i)
-      normal.fromBufferAttribute(normalAttribute, i)
-      const v = vertex.multiplyScalar(0.5)
-      const n = Perlin.simplex3(...v.toArray())
-      vertex.add(normal.multiplyScalar(n * 0.3))
-      newPositionAttribute.push(vertex.x, vertex.y, vertex.z)
-    }
-    geom.current.setAttribute('position', new THREE.Float32BufferAttribute(newPositionAttribute, 3))
-    geom.current.attributes.position.needsUpdate = true
-    geom.current.computeVertexNormals()
-  }, [])
+  const fillMaterial = new THREE.MeshBasicMaterial({ color: '#F3FBFB' })
+  const stokeMaterial = new THREE.LineBasicMaterial({
+    color: '#00A5E6'
+  })
+
+  const loader = new SVGLoader()
+  const svgUrl = '../static/objects/small-map.svg'
+  const svgGroup = new THREE.Group()
+  const updateMap = []
+  svgGroup.scale.y *= -1
+
+  loader.load(svgUrl, (data) => {
+    data.paths.forEach((path) => {
+      const shapes = path.toShapes(true)
+
+      shapes.forEach((shape) => {
+        const meshGeometry = new THREE.ExtrudeBufferGeometry(shape, {
+          depth: 20,
+          bevelEnabled: false
+        })
+        const linesGeometry = new THREE.EdgesGeometry(meshGeometry)
+        const mesh = new THREE.Mesh(meshGeometry, fillMaterial)
+        const lines = new THREE.LineSegments(linesGeometry, stokeMaterial)
+
+        updateMap.push({ shape, mesh, lines })
+        svgGroup.add(mesh, lines)
+      })
+    })
+  })
+
+  useEffect(() => {}, [])
 
   return (
     <mesh ref={ref}>
-      <icosahedronBufferGeometry args={[1.5, 16]} ref={geom}>
-        <ComputedAttribute name="density" compute={computeFlowerDensity} usage={THREE.StaticReadUsage} />
-      </icosahedronBufferGeometry>
+      <boxGeometry ref={geom}></boxGeometry>
       <meshBasicMaterial color="#221600" />
     </mesh>
   )
